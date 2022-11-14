72f67402c4829203cc9d4e17ac915e6eac7941e0
rkkocheran <rkkocheran@gmail.com>
2022-11-14 16:34:50 -0600

    updated contributions and formatted the final analysis

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-### App Trader
+### App Trader Project
 
+#### *This project was a team effort with contributions from Adam Crank and Morgan Hines.
+#### *All analyses are done in PostgreSQL, however query results are exported to create charts in Excel for the report.
+
+#### Premise:
 Your team has been hired by a new company called App Trader to help them explore and gain insights from apps that are made available through the Apple App Store and Android Play Store. App Trader is a broker that purchases the rights to apps from developers in order to market the apps and offer in-app purchase. App developers retain **all** money from users purchasing the app, and they retain _half_ of the money made from in-app purchases. App Trader will be solely responsible for marketing apps they purchase rights to.  
 
 Unfortunately, the data for Apple App Store apps and Android Play Store Apps is located in separate tables with no referential integrity.
 
-#### 1. Loading the data
+#### Data Load-in:
 a. Launch PgAdmin and create a new database called app_trader.  
 
 b. Right-click on the app_trader database and choose `Restore...`  
@@ -15,13 +19,13 @@ d. In the `Filename` section, browse to the backup file `app_store_backup.backup
 
 e. Click `Restore` to load the database.  
 
-f. Verify that you have two tables:  
+f. Verify there are two tables:  
     - `app_store_apps` with 7197 rows  
     - `play_store_apps` with 10840 rows
 
 
-#### 2. Assumptions
-Based on research completed prior to launching App Trader as a company, you can assume the following:  
+#### Assumptions:
+Based on research completed prior to launching App Trader as a company, the following can be assumed:  
 
 a. App Trader will purchase apps for 10,000 times the price of the app, however the minimum price to purchase an app is $25,000.  For example, a $3 app would cost $30,000 (10,000 x the price) and a free app would cost $25,000 (The minimum price).  NO APP WILL EVER COST LESS THEN $25,000 TO PURCHASE.  
 
@@ -33,14 +37,9 @@ d. For every quarter-point that an app gains in rating, its projected lifespan i
 
 e. App Trader would prefer to work with apps that are available in both the App Store and the Play Store since they can market both for the same $1000 per month.
 
-#### 3. Deliverables
+#### Deliverables:
 a. Develop some general recommendations as to the price range, genre, content rating, or anything else for apps that the company should target.  
 
 b. Develop a Top 10 List of the apps that App Trader should buy next week for its **Halloween** debut.  
 
-c. Submit a report based on your findings.  The report should include the 10 apps which would have the highest return on investment.  Also, select 5 profitable apps
-which fit the theme; also include their cost and potential profits.
-
-
-
-### All analysis work must be done in PostgreSQL, however you may export query results to create charts in Excel for your report.
+c. Submit a report based on the team's findings.  The report should include the 10 apps which would have the highest return on investment.  Also, select 5 profitable apps which fit the theme; also include their cost and potential profits.
---
+++ app_trader_analysis.sql
@@ -0,0 +1,177 @@
+/* Project Name: App Trader Analysis
+   Project Team: Rachel Kocheran, Morgan Hines, Adam Crank */
+
+
+
+
+/* Analysis for the most popular apps regarding content_rating in the Android Play Store.
+   Conclusion: Over 80% of Android Play App Customers are in the 'Everyone' category 
+   with 'Teen' constituting about 11%, together these categories make up over 90%
+   of Android Play Store's consumer market. */
+   
+SELECT content_rating,
+       ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER(), 0) AS percentage_total
+FROM (SELECT DISTINCT *
+      FROM play_store_apps
+      ORDER BY name) AS distinct_play_table
+GROUP BY content_rating
+ORDER BY percentage_total DESC;
+
+
+/* Analysis for the most popular apps regarding content_rating in the Apple App Store.
+   Conclusion: Over 60% of Apple App Customers are in the '4+' category 
+   with '12+' constituting about 16%, together these categories make up almost 80%
+   of Android Play Store's consumer market. */ 
+   
+SELECT content_rating,
+       ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER(), 0) AS percentage_total
+FROM (SELECT DISTINCT *
+      FROM app_store_apps
+      ORDER BY name) AS distinct_app_table
+GROUP BY content_rating
+ORDER BY percentage_total DESC;
+
+
+
+
+/* Analysis for most popular category in the Android Play Store.
+   Conclusion: Almost 20% of Android Play App apps fall in the 'Family' category 
+   with 'Game' constituting about 11%, together these categories make up almost 30%
+   of Android Play Store's consumer market. */
+   
+SELECT category,
+       ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER(), 0) AS percentage_total
+FROM (SELECT DISTINCT *
+      FROM play_store_apps
+      ORDER BY name) AS distinct_play_table
+GROUP BY category
+ORDER BY percentage_total DESC
+LIMIT 5;
+
+
+/* Analysis for most popular category in the Apple App Store.
+   Conclusion: Over 50% of Apple's App Store apps fall in the 'Games' category 
+   with 'Entertainment' constituting about 7% of Android Play Store's consumer market. */
+   
+SELECT primary_genre,
+       ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER(), 0) AS percentage_total
+FROM (SELECT DISTINCT *
+      FROM app_store_apps
+      ORDER BY name) AS distinct_app_table
+GROUP BY primary_genre
+ORDER BY percentage_total DESC
+LIMIT 5;
+
+
+
+
+/* Establish a temporary table which joins both the app and play store databases, removes unnecessary duplicates
+   and adds calucated columns for each distinct app's average price, rating, review_count, and purchase price with a final
+   calculated column for each app's estimated lifespan. */
+   
+WITH distinct_appstores AS (    
+	SELECT DISTINCT name,
+	 	 app_store_apps.price::MONEY AS app_price,
+  	 	 app_store_apps.rating AS app_rating,
+ 	 	 app_store_apps.primary_genre AS app_genre,
+ 	 	 app_store_apps.review_count::INTEGER AS app_review_count,
+	 	 app_store_apps.content_rating AS app_content_rating,
+	 	 play_store_apps.price::MONEY AS play_price,
+	 	 play_store_apps.rating AS play_rating,
+	 	 play_store_apps.category AS play_genre,
+	 	 play_store_apps.review_count AS play_review_count,
+	 	 play_store_apps.content_rating AS play_content_rating,
+ 	
+	 	 (app_store_apps.price::MONEY + play_store_apps.price::MONEY)/2 AS avg_price,
+	 	 ROUND(((app_store_apps.rating + play_store_apps.rating)/2)/25,2)*25 AS avg_rating,
+	 	 (app_store_apps.review_count::INTEGER + play_store_apps.review_count)/2 AS avg_review_count,
+	 	 2*(ROUND(((app_store_apps.rating + play_store_apps.rating)/2)/25,2)*25)+1 AS lifespan_yrs,
+	
+	 	 CASE WHEN ((app_store_apps.price::MONEY + play_store_apps.price::MONEY)/2)::NUMERIC >=2.5
+                  THEN (((app_store_apps.price::MONEY + play_store_apps.price::MONEY)/2)::NUMERIC *10000)
+	 	      ELSE 25000
+                  END AS avg_price_to_purchase
+	FROM app_store_apps 
+	INNER JOIN play_store_apps 
+      USING(name)
+)
+
+/* Determine the best recommendations for App Trader using the most popular content ratings, genres and price brackets,
+   and filter the results to return the Top-10 most profitable apps based on the calculated fields for net profit over each 
+   app's estimated lifespan. */
+
+SELECT DISTINCT(name),
+	 avg_price,
+	 avg_rating,
+	 avg_price_to_purchase::MONEY,
+	 (avg_price_to_purchase + (1000*(lifespan_yrs*12)))::MONEY AS gross_cost,
+	 ((5000*(lifespan_yrs*12)))::MONEY AS gross_profit,
+	 (((5000*(lifespan_yrs*12))) - (avg_price_to_purchase + (1000*(lifespan_yrs*12))))::MONEY AS net_profit
+FROM distinct_appstores
+WHERE avg_price::NUMERIC < 2 
+      AND app_genre ILIKE('%game%') 
+      AND play_genre ILIKE('%game%')
+	AND app_content_rating = '4+' 
+      AND play_content_rating = 'Everyone'
+ORDER BY net_profit DESC
+LIMIT 10;
+
+
+
+
+/* CTEs for determining a list of top-10 app recommendations for Halloween 
+   based on profitability and theme. */
+
+WITH distinct_appstores AS (
+	SELECT DISTINCT name,
+                      app_store_apps.price::MONEY AS app_price,
+                      app_store_apps.rating AS app_rating,
+                      app_store_apps.primary_genre AS app_genre,
+                      app_store_apps.review_count::INTEGER AS app_review_count,
+                      app_store_apps.content_rating AS app_content_rating,
+                      play_store_apps.price::MONEY AS play_price,
+                      play_store_apps.rating AS play_rating,
+                      play_store_apps.category AS play_genre,
+                      play_store_apps.review_count AS play_review_count,
+                      play_store_apps.content_rating AS play_content_rating,
+                      (app_store_apps.price::MONEY + play_store_apps.price::MONEY) / 2 AS avg_price,
+                      ROUND(((app_store_apps.rating + play_store_apps.rating) / 2) / 25, 2) * 25 AS avg_rating,
+                      (app_store_apps.review_count::INTEGER + play_store_apps.review_count) / 2 AS avg_review_count,
+                      2 * (ROUND(((app_store_apps.rating + play_store_apps.rating) / 2) / 25, 2) * 25) + 1 AS lifespan_yrs,
+                      CASE WHEN ((app_store_apps.price::MONEY + play_store_apps.price::MONEY) / 2)::NUMERIC >= 2.5 
+                           THEN (((app_store_apps.price::MONEY + play_store_apps.price::MONEY) / 2)::NUMERIC * 10000)
+                           ELSE 25000
+                           END AS avg_price_to_purchase
+	FROM app_store_apps
+	INNER JOIN play_store_apps 
+      USING(name)
+),
+
+halloween_apps AS (
+      SELECT distinct(name),
+             avg_price,
+             avg_rating,
+             (avg_price_to_purchase)::MONEY,
+             (avg_price_to_purchase + (1000 * (lifespan_yrs * 12)))::MONEY AS gross_cost,
+             ((5000 * (lifespan_yrs * 12)))::MONEY AS gross_profit,
+             (((5000 * (lifespan_yrs * 12))) - (avg_price_to_purchase + (1000 * (lifespan_yrs * 12))))::MONEY AS net_profit
+      FROM distinct_appstores
+      WHERE ((5000 * (lifespan_yrs * 12))) - (avg_price_to_purchase + (1000 * (lifespan_yrs * 12))) > 0
+            AND app_genre ILIKE('%game%')
+            AND play_genre ILIKE('%game%')
+            AND name ILIKE '%zombie%'
+            OR name ILIKE '%candy%'
+            OR name ILIKE '%five%'
+            OR name ILIKE '%temple%'
+      ORDER BY net_profit DESC
+)
+
+/* Filter for the Top-5 Halloween app recommendations using the above two CTEs. */
+
+SELECT *
+FROM halloween_apps
+WHERE name = 'Zombie Catchers'
+      OR name = 'Candy Crush Saga'
+      OR name = 'Temple Run'
+      OR name = 'Zombie Tsunami'
+      OR name = 'Five Nights at Freddy''s';
